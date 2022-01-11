2a5e7b7b1914da9406a9b51e0a3956db7200e71b
elijahzhang2021 <77467304+elijahzhang2021@users.noreply.github.com>
2022-01-11 16:27:11 +1100

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,24 @@
+##load package
+library(dplyr)
+
+##read data from file
+data <- read.table("./household_power_consumption.txt", sep = ";", na.strings="?", header=TRUE)
+
+##add column with POSIXct date/time
+
+data$DateTime <- as.POSIXct(strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S"))
+
+##filter only data containing relevant days using Date column
+
+plot_data <- filter(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+
+##create plot 3
+
+png("plot3.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(4,4,4,2))
+plot(plot_data$DateTime, plot_data$Sub_metering_1, type="l", xlab = NA, ylab = "Energy sub metering")
+lines(plot_data$DateTime, plot_data$Sub_metering_2, type="l", col = "red")
+lines(plot_data$DateTime, plot_data$Sub_metering_3, type="l", col = "blue")
+legend("topright", legend = c(names(plot_data)[7:9]), col = c("black", "red", "blue"), lty=1, cex=0.8)
+dev.off()
